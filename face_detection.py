170d8bbaaae2cb19252a1560418b3c2f3c1de1a8
Dhanush Nayak <40546930+dhanushnayak@users.noreply.github.com>
2020-04-06 11:28:58 +0530

    Python File

--- face_detection.py
+++ face_detection.py
@@ -36,7 +36,7 @@ while 1:
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-            print(matches)
+            
             name = "Unknown"
 
             # # If a match was found in known_face_encodings, just use the first one.
